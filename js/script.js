62bd718114f5d18419204656dbfa3a20339baf9e
backdatedb4ckd00r <towsifseyam13@gmail.com>
2021-01-08 17:01:29 +0600

    uploaded again

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function aboutUs(){
+	document.getElementById('about').innerHTML = "The Notaries Society is the representative society for the 770 or so Notaries Public practising in England and Wales. From this page you will find various topics of interest to members of the profession and the public alike.";
+}
+function textBlank(){
+	document.getElementById('about').innerHTML = " ";
+}
+
